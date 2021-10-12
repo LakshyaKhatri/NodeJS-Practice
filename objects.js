33a1597f7dcda1e295473863a51d0aba9b869c49
Lakshya Khatri <lakshyak67@gmail.com>
2021-10-12 19:15:05 +0530

    Add solution for: https://javascript.info/task/multiply-numeric

--- objects.js
+++ objects.js
@@ -38,3 +38,19 @@ let salaries = {
 };
 
 sumAll(salaries)
+
+// Problem: https://javascript.info/task/multiply-numeric
+function multiplyNumeric(obj) {
+  for (key in obj) {
+    if (typeof obj[key] === 'number') obj[key] *= 2
+  }
+}
+
+let menu = {
+  width: 200,
+  height: 300,
+  title: "My menu"
+};
+
+multiplyNumeric(menu);
+console.log(menu);

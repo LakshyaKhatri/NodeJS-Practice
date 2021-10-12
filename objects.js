8448bfb40c3afb4c60f5e957f289296ab9b92215
Lakshya Khatri <lakshyak67@gmail.com>
2021-10-12 19:10:39 +0530

    Add solutions for problems based on objects listed on js info

---
+++ objects.js
@@ -0,0 +1,40 @@
+// Problem: https://javascript.info/task/hello-object
+
+let user = {};
+user.name = 'John';
+user.surname = 'Smith';
+user.name = 'Pete';
+delete user.name;
+
+
+// Problem: https://javascript.info/task/is-empty
+
+function isEmpty(obj) {
+  for(key in obj) {
+    // if loop starts then there's a key in the object
+    return false;
+  }
+  return true;
+}
+
+let schedule = {};
+console.log( isEmpty(schedule) );
+schedule["8:30"] = "get up";
+console.log( isEmpty(schedule) );
+
+// Problem: https://javascript.info/task/sum-object
+function sumAll(obj) {
+  let sum = 0;
+  for(key in obj) {
+    sum += obj[key];
+  }
+  return sum;
+}
+
+let salaries = {
+  John: 100,
+  Ann: 160,
+  Pete: 130
+};
+
+sumAll(salaries)

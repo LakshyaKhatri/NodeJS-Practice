6cfef01cc3088ec2e5d58abffd1bd6c812ad632d
Lakshya Khatri <lakshyak67@gmail.com>
2021-10-13 16:02:32 +0530

    Fix: uncomment temporarily commented code

--- obj_methods_and_this.js
+++ obj_methods_and_this.js
@@ -11,7 +11,7 @@ function makeUser() {
 
 let user = makeUser();
 
-// console.log( user.ref.name ); // What's the result?
+console.log( user.ref.name ); // What's the result?
 // The above line will throw an error because:
 // The function is not assigned to any object and we are in strict mode. Hence, the value
 // of `this` will be `undefined` and calling `.name` on undefined will throw this error:
@@ -40,9 +40,9 @@ let calculator = {
   }
 };
 
-// console.log(calculator.read());
-// console.log(calculator.sum());
-// console.log(calculator.mul());
+console.log(calculator.read());
+console.log(calculator.sum());
+console.log(calculator.mul());
 
 
 // Problem: https://javascript.info/task/chain-calls

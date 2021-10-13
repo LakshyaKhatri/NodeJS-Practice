3d177f9282fdebeab5e6922202c6ccabd811a98e
Lakshya Khatri <lakshyak67@gmail.com>
2021-10-13 16:00:27 +0530

    Add solution for '2 constructor one object problem'

---
+++ constructor_new.js
@@ -0,0 +1,12 @@
+'use strict'
+
+// Problem: Is it possible to create functions A and B so that new A() == new B()?
+let obj = {};
+
+function A() { return obj; }
+function B() { return obj; }
+
+let a = new A;
+let b = new B;
+
+console.log( a == b ); // true

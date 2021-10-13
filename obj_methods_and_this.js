f7d693244a082d04ab38653daf0cdc6e95221379
Lakshya Khatri <lakshyak67@gmail.com>
2021-10-13 13:36:19 +0530

    Add solution for `this` problem: https://javascript.info/task/calculator

--- obj_methods_and_this.js
+++ obj_methods_and_this.js
@@ -1,4 +1,5 @@
 'use strict'
+const prompt = require("prompt-sync")();
 
 // Problem: https://javascript.info/task/object-property-this
 function makeUser() {
@@ -10,8 +11,35 @@ function makeUser() {
 
 let user = makeUser();
 
-console.log( user.ref.name ); // What's the result?
+// console.log( user.ref.name ); // What's the result?
 // The above line will throw an error because:
 // The function is not assigned to any object and we are in strict mode. Hence, the value
 // of `this` will be `undefined` and calling `.name` on undefined will throw this error:
 // Cannot read property 'name' of undefined.
+
+
+// Problem: https://javascript.info/task/calculator
+let calculator = {
+  read(){
+    this.a = +(prompt('Enter first number: '));
+    this.b = +(prompt('Enter second number: '));
+  },
+  sum(){
+    if (this.a === undefined) {
+      console.log('Use calculator.read() to register input values first');
+      return;
+    }
+    return (this.a + this.b);
+  },
+  mul(){
+    if (this.a === undefined) {
+      console.log('Use calculator.read() to register input values first');
+      return;
+    }
+    return (this.a * this.b);
+  }
+};
+
+console.log(calculator.read());
+console.log(calculator.sum());
+console.log(calculator.mul());

f0df5e775cd018f1377d99467fdb91fb9c76f6da
Lakshya Khatri <lakshyak67@gmail.com>
2021-10-26 12:48:54 +0530

    Add solution for Assignment 2 Question 3

---
+++ assignment_2/solution_3.js
@@ -0,0 +1,14 @@
+/*
+================================================================================
+ * Problem: Write a regex pattern to match the valid email address
+================================================================================
+*/
+
+let cardNumberRegex = /^\d{4}((\-| )?)\d{4}\1\d{4}\1\d{4}/;
+
+// Driver Code
+console.log(cardNumberRegex.test('1234 5678 9123 4564'));
+console.log(cardNumberRegex.test('1234-5678-9123-4564'));
+console.log(cardNumberRegex.test('1234567891234564'));
+console.log(cardNumberRegex.test('12345678-9123-4564'));
+console.log(cardNumberRegex.test('1234 5678-9123 4564'));
